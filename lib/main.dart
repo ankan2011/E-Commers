ed9af93b7ccd2dce37ab6b4d0404afd7b1785b27
ankan2011 <ankanpanja2011@gmail.com>
2026-05-28 09:08:05 +0530

    splash_screen created

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 import 'my_app.dart';
 
 void main() {
+  //Widget flutter binding
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  //Flutter native splash
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(const MyApp());
 }
 
